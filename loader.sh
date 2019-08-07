3cc0ade34801dcdbb069a2af77a07d74a30f75c1
Juan manuel Pedro Villalba <xomie_01@hotmail.com>
2019-08-07 10:57:11 -0300

    Descargador de producción
    Crea una descarga del archivo de producción de install.php para realizar despliegue fast-speed y ahorrar horas de trabajos
    Ahorro de tiempo en descargar repositorios de alta utilidad.

---
+++ loader.sh
@@ -0,0 +1,10 @@
+
+download-installer()
+{
+wget https://bitbucket.org/servitec2018/init_cloud/raw/d4359b26dcfaa0ceb45df2566db57be28d489e1a/install-update.php
+cp install-update.php
+bin/install.php
+php bin/install.php
+}
+
+download-installer
